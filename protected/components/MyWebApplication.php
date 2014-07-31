a0a17ee9697322e74131d00324afb71fffdbf138
khalidmnet <khalid@khalidm.net>
2014-07-31 19:05:16 +0300

    Fix formatting of code

--- protected/components/MyWebApplication.php
+++ protected/components/MyWebApplication.php
@@ -3,49 +3,46 @@
 /**
  * CWebApplication overridden to add functionality
  */
-class MyWebApplication extends CWebApplication
-{
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			$route = Yii::app()->request->requestUri;
+class MyWebApplication extends CWebApplication {
+
+    public function beforeControllerAction($controller, $action) {
+        if (parent::beforeControllerAction($controller, $action)) {
+            $route = Yii::app()->request->requestUri;
             $userType = $userId = "";
-            
+
             $browser = $_SERVER['HTTP_USER_AGENT'];
             $ipAddress = $_SERVER['REMOTE_ADDR'];
-            
+
             //If user is logged in to the front end, log his info
-            if(isset(Yii::app()->user->ut)){
-            	if(Yii::app()->user->ut == 'user'){
-            		$userType = "User";
-            		$userId = Yii::app()->user->getId();
-            	}
+            if (isset(Yii::app()->user->ut)) {
+                if (Yii::app()->user->ut == 'user') {
+                    $userType = "User";
+                    $userId = Yii::app()->user->getId();
+                }
+            } else {//Else, give him unique guest ID session to log his movements
+                if (!isset(Yii::app()->user->guestId)) {
+                    Yii::app()->user->setState('guestId', time() . rand());
+                }
+
+                $userType = "Guest";
+                $userId = Yii::app()->user->guestId;
             }
-            else
-            {//Else, give him unique guest ID session to log his movements
-            	if(!isset(Yii::app()->user->guestId)){
-            		Yii::app()->user->setState('guestId',time().rand());
-            	}
-            	
-            	$userType = "Guest";
-            	$userId = Yii::app()->user->guestId;
-            }
-            
+
             //Now insert logged info
             $activity = new Useractivity();
             $activity->activity_user_type = $userType;
             $activity->activity_user_id = $userId;
-            $activity->activity_user_ip	= $ipAddress;
+            $activity->activity_user_ip = $ipAddress;
             $activity->activity_user_browser = $browser;
             $activity->activity_route = $route;
             $activity->activity_datetime = new CDbExpression('NOW()');
             $activity->save();
-            
-            
+
+
             return true;
-		}
-		else
-			return false;
-	}
+        }
+        else
+            return false;
+    }
+
 }
